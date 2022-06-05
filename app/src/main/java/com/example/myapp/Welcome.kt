3c795909d120649f362063cc48ab4f84fdc1fa52
tarundev1994 <62416363+tarundev1994@users.noreply.github.com>
2022-06-05 16:33:01 +1000

    Intent

--- app/src/main/java/com/example/myapp/Welcome.kt
+++ app/src/main/java/com/example/myapp/Welcome.kt
@@ -3,7 +3,7 @@ package com.example.myapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class Welcome :AppCompatActivity() {
+class Welcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_screen)

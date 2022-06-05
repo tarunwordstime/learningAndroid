ae4e44079df87ee0533a3887997a4f4ad32c04f9
tarundev1994 <62416363+tarundev1994@users.noreply.github.com>
2022-06-05 16:50:47 +1000

    Intent and Spinner Done

--- app/src/main/java/com/example/myapp/Welcome.kt
+++ app/src/main/java/com/example/myapp/Welcome.kt
@@ -2,10 +2,35 @@ package com.example.myapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
-class Welcome : AppCompatActivity() {
+class Welcome : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    var courses = arrayOf("Php", "C", "C++", "Java", "Android")
+    lateinit var spinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_screen)
+        spinner = findViewById(R.id.spinner)
+        spinner.onItemSelectedListener = this
+        val ad: ArrayAdapter<*> = ArrayAdapter<Any?>(
+            this,
+            android.R.layout.simple_spinner_item,
+            courses
+        )
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = ad
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Toast.makeText(this, courses[position], Toast.LENGTH_LONG).show()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        Toast.makeText(this, "You have not selected!", Toast.LENGTH_LONG).show()
     }
 }
